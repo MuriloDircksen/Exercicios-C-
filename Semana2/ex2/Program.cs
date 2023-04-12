1e58bc73cde20416fa0584a9ae70e4b3da1b392b
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 16:33:42 -0300

    solução ex2 semana 2

---
+++ Semana2/ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro!");
+            int n = int.Parse(Console.ReadLine());
+
+            if(n%2 == 0)
+            {
+                Console.WriteLine("Número par digitado!");
+            }
+            else
+            {
+                Console.WriteLine("Número ímpar digitado!");
+            }
+        }
+    }
+}
