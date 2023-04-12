07d34606cd20b581e46c083b83121e6973dbb12c
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 17:41:05 -0300

    solução ex9 semana 2

---
+++ Semana2/ex9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+            Console.WriteLine("Digite a quantidade de números para executar a média aritimética!");
+            int qtd = int.Parse(Console.ReadLine());
+            int[] numeros = new int[qtd];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write((i + 1) + " :");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            foreach(int numero in numeros)
+            {
+                soma += numero;
+            }
+            Console.WriteLine("Média aritimética: "+(soma/qtd));
+        }
+    }
+}
