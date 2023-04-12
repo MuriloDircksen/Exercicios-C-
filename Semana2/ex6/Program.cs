da75ea133ad8d090be8aef5a0ef85fd5b83cdb92
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 17:10:52 -0300

    solução ex6 semana 2

---
+++ Semana2/ex6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase!");
+            String frase = Console.ReadLine();
+            String[] palavras = frase.Split(" ");
+
+            foreach(String palavra in palavras)
+            {
+                if(palavra == "poderosa")
+                {
+                    Console.WriteLine(palavra);
+                }
+            }
+        }
+    }
+}
