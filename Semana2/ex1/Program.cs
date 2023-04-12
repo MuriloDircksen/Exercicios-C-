49213926cb29ff449ab01eea2aec4e666aff2490
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 16:17:00 -0300

    solução ex1 semana 2

---
+++ Semana2/ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números inteiros!");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("A soma deles é: "+ (n1+n2));
+        }
+    }
+}
