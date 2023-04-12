901e3ab24a50b52ffdf7d3f722aae1f50dc5afe0
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 16:51:20 -0300

    solução ex4 semana 2

---
+++ Semana2/ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a marca do veiculo!");
+            String marca = Console.ReadLine();
+            Console.WriteLine("Digite o modelo do veiculo!");
+            String modelo = Console.ReadLine();
+            Console.WriteLine("Digite o número de km rodados!");
+            int km = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veiculo de marca " + marca + ", modelo " + modelo);
+            if (km >= 10000)
+            {
+                Console.WriteLine("Precisa fazer revisão!");
+            }
+        }
+    }
+}
