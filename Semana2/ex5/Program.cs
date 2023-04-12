ca6d571cdd928472747233e807c9b6419da4a33f
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 17:04:09 -0300

    solução ex5 semana 2

---
+++ Semana2/ex5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 4 números inteiros!");
+            int[] numeros = new int[4];
+
+            for(int i = 0; i < 4; i++) {
+                Console.WriteLine($"Digite o numero {i+1}");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(numeros);
+
+            Console.WriteLine("Números ordenados:");
+            foreach(int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
