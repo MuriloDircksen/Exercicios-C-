659056ea3047bf302114fd1252b22d7d23101ef2
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 16:43:36 -0300

    solução ex3 semana 2

---
+++ Semana2/ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do paciente!");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade do paciente!");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome do paciente: " + nome);
+
+            if (idade < 18) { 
+                Console.WriteLine("Menor de idade: " +  idade + " anos");
+            }
+            else if (idade >= 60)
+            {
+                Console.WriteLine("Idoso");
+                Console.WriteLine("Maior de idade: " + idade + " anos");
+            }
+            else
+            {
+                Console.WriteLine("Maior de idade: " + idade + " anos"); 
+            }
+        }
+    }
+}
