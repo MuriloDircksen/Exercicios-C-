c3eae821eb4de31a8cd852e971fe29c1ba4ec186
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 17:22:36 -0300

    solução ex7 semana 2

---
+++ Semana2/ex7/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numero = 0;
+            do
+            {
+                Console.WriteLine("Digite um número positivo!");
+                numero = int.Parse(Console.ReadLine());
+                if(numero > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Erro, número inválido digitado");
+
+            } while (true);
+
+            Console.WriteLine("Números pares:");
+
+            for(int i = 0; i < numero; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
