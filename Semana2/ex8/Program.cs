1e98ca01593a5481a4ca45770e49c6dca18a6c34
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-12 17:32:17 -0300

    solução ex8 semana 2

---
+++ Semana2/ex8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+            int numero = 0;
+            Console.WriteLine("Digite cinco números!");
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write((i+1)+" :");
+                numeros[i] = int.Parse(Console.ReadLine());
+                if (numeros[i] > numero)
+                {
+                    numero = numeros[i];
+                }
+            }
+            Console.WriteLine("Maior número: " + numero);
+            
+        }
+    }
+}
