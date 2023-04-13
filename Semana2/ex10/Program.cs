7220d062d73d6f54bc1d0d632a6d03a2ec0e07a4
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-13 15:21:05 -0300

    solução ex10 semana 2

---
+++ Semana2/ex10/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            char variavel = 'a';
+            double n1 = 0, n2 = 0;
+           
+            do
+            {
+                Console.WriteLine("Escolha a operação a ser executada:\n+: Soma\n-: Subtração\n*: Multiplicação\n/: Divisão\n?: Sair ");
+                variavel = char.Parse(Console.ReadLine());
+                bool controle = true;
+                double resultado = 0;
+                if (variavel != '?')
+                {
+                    
+                    Console.WriteLine("Digite o primeiro número do calculo");
+                    n1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo número do calculo");
+                    n2 = int.Parse(Console.ReadLine());
+               
+                    switch (variavel)
+                    {
+                        case '+':
+                            resultado = n1 + n2;
+                            break;
+                        case '-':
+                            resultado = n1 - n2;
+                            break;
+                        case '*':
+                            resultado = n1 * n2;
+                            break;
+                        case '/':
+                            if (n2 == 0)
+                            {
+                                Console.WriteLine("Não é possível dividir por zero!");
+                                break;
+                            }
+                            resultado = n1 / n2;
+                            break;
+                        case '?':
+                        
+                            break;
+                        default:
+                            Console.WriteLine("Insira um valor correto!");
+                            controle = false;
+                            break;
+
+                    }
+                    if (controle)
+                    {
+                        Console.WriteLine($"Resultado: {resultado}\n\n");
+                    }
+                }
+            } while (variavel != '?');
+            Console.WriteLine("Obrigado por usar a Calculadora!");
+        }
+    }
+}
