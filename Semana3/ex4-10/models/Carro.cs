9bff632e32eac34e88f547a954251660a21394c3
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-19 15:21:42 -0300

    solução ex5 semana 3

---
+++ Semana3/ex4-10/models/Carro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4_10.models
+{
+    internal class Carro
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+        public Proprietario Proprietario { get; set; }
+    }
+}
