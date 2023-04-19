bcdcd17af4b4ae027b575e364b5b365b29b4f7bc
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-19 14:24:00 -0300

    solução ex2 semana 3

--- Semana3/ex1-2/Program.cs
+++ Semana3/ex1-2/Program.cs
@@ -5,10 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Soma número 2 e 3: " + Soma(2, 3));
+            Console.WriteLine("Soma número 2,3 e 3,2: " + Soma(2.3, 3.2));
+            Console.WriteLine("Soma número 2,3, 3,2 e 4,1: " + Soma(2.3, 3.2, 4.1));
         }
 
         static int Soma(int n1, int n2) {
             return n1 + n2;
         }
+
+        static double Soma(double n1, double n2) {  return n1 + n2; }
+
+        static double Soma(double n1, double n2, double n3) { return n1 + n2 + n3; }
     }
 }
