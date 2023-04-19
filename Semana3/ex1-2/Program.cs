dd6146a426231b1631fd996791987f04055a4ed0
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-19 14:18:50 -0300

    solução ex1 semana 3

---
+++ Semana3/ex1-2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ex1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soma número 2 e 3: " + Soma(2, 3));
+        }
+
+        static int Soma(int n1, int n2) {
+            return n1 + n2;
+        }
+    }
+}
