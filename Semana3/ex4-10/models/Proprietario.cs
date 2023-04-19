e4e745e2f9021585150cf872aa0dc4c1dd5a4176
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-19 14:40:13 -0300

    solução ex3 semana 3

---
+++ Semana3/ex4-10/models/Proprietario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4_10.models
+{
+    internal class Proprietario
+    {
+        public string nome { get; set; }
+        public string cpf { get; set;}
+        public string Telefone { get; set;}
+    }
+}
