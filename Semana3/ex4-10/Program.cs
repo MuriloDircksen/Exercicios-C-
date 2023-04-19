dbf2fcf86ccba72fb1b3f4a7b4d9b9ea24abfa75
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-19 15:46:25 -0300

    solução ex8 semana 3

--- Semana3/ex4-10/Program.cs
+++ Semana3/ex4-10/Program.cs
@@ -61,6 +61,48 @@ namespace ex4_10
             Carros.Add(carro1);
             Carros.Add(carro2);
             Carros.Add(carro3);
+            CadastrarNovoCarro(Carros);
+            
+            foreach(var carro in Carros)
+            {
+                Console.WriteLine(carro.Placa);
+            }
+        }
+
+        static void CadastrarNovoCarro(List<Carro> Carros)
+        {
+            Console.WriteLine("Cadastro novo carro.");
+            Console.WriteLine("Digite o nome do Proprietario:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o CPF do Proprietario:");
+            string cpf = Console.ReadLine();
+            Console.WriteLine("Digite o Telefone do Proprietario:");
+            string telefone = Console.ReadLine();
+            var proprietario = new Proprietario()
+            {
+                Nome = nome,
+                Cpf = cpf,
+                Telefone = telefone
+
+            };
+
+            Console.WriteLine("Digite a marca do carro:");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Digite o modelo do carro:");
+            string modelo = Console.ReadLine();
+            Console.WriteLine("Digite a placa do carro:");
+            string placa = Console.ReadLine();
+            Console.WriteLine("Digite a cor do carro:");
+            string cor = Console.ReadLine();
+            var carro = new Carro()
+            {
+                Marca = marca,
+                Modelo = modelo,
+                Placa = placa,
+                Cor = cor,
+                Proprietario = proprietario
+            };
+            Carros.Add(carro);
         }
     }
 }
