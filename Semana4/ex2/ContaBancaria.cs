f8adf4abb9c0824ff28f648a3ad4a12a4c9cb799
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-25 19:29:47 -0300

    ex2 da semana 4

---
+++ Semana4/ex2/ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    internal class ContaBancaria
+    {
+        private int NumeroDaConta;
+        private double Saldo;
+
+        public ContaBancaria(int numeroDaConta, double saldo)
+        {
+            NumeroDaConta = numeroDaConta;
+            Saldo = saldo;
+        }
+
+        public void Deposita(double valor)
+        {
+            if (valor >= 0)
+            {
+                Saldo += valor;
+            }
+           
+        }
+
+        public void Saca(double valor)
+        {
+            if (valor >= 0 && valor <= Saldo)
+            {
+                Saldo -= valor;
+            }
+            Console.WriteLine("O valor solicitado e maior que o saldo disponível ou negativo!");
+        }
+        public double GetSaldo()
+        {
+            return Saldo;
+        }
+    }
+}
---
+++ Semana4/ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria  contaBancaria1 = new ContaBancaria(1234, 200.00);
+
+            contaBancaria1.Deposita(100.00);
+            contaBancaria1.Saca(50.00);
+            Console.WriteLine("Saldo conta bancaria: "+contaBancaria1.GetSaldo());
+        }
+    }
+}
