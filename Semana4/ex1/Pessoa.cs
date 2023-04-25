4cfb2f1cefa076074066e0d989bc21fbffb8407b
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-25 19:15:26 -0300

    ex1 da semana 4

---
+++ Semana4/ex1/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    internal class Pessoa
+    {
+        private string Nome;
+        protected int Idade;
+        public string Endereco;
+
+        public Pessoa(string nome, int idade, string endereco)
+        {
+            Nome = nome;
+            Idade = idade;
+            Endereco = endereco;
+        }
+
+        public string Saudacao() {
+            return $"Olá, meu nome é {Nome} e eu tenho {Idade} anos";
+        }
+    }
+}
---
+++ Semana4/ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("Joana", 23, "Rua das flores"); 
+            Console.WriteLine(pessoa1.Saudacao());
+        }
+    }
+}
