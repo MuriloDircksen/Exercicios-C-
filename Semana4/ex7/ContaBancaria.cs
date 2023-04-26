7e7e2f649fe7508b8869680aba44fc931a28a8e0
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-26 09:47:03 -0300

    ex7 da semana 4

---
+++ Semana4/ex7/ContaBancaria.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7
+{
+    internal class ContaBancaria
+    {
+        public const double SaldoMax = 1000.00;
+        private int _numeroDaConta;
+        private double _saldo;
+
+        public double Saldo { get => _saldo; set => _saldo = value; }
+
+        public void Depositar(double valor)
+        {            
+            if (valor < 0 || (valor+Saldo) > SaldoMax)
+            {
+                Console.WriteLine("Valor de deposito não permitido!");
+            }
+            else { Saldo += valor; }
+        }
+        public void Sacar(double valor)
+        {
+            if (valor < 0 && valor > Saldo)
+            {
+                Console.WriteLine("Valor de saque não permitido!");
+            }
+            else { Saldo -= valor; }
+        }
+    }
+}
---
+++ Semana4/ex7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria();
+            contaBancaria.Saldo = 10.00;
+            Console.WriteLine("Deposito de 1000");
+            contaBancaria.Depositar(1000.00) ;
+            contaBancaria.Depositar(500.00);
+            Console.WriteLine("Deposito de 500");
+            Console.WriteLine("Saldo: " + contaBancaria.Saldo);
+            Console.WriteLine("Saque de 100");
+            contaBancaria.Sacar(100.00);
+            Console.WriteLine("Saldo: " + contaBancaria.Saldo);
+        }
+    }
+}
