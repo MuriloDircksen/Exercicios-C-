691469a3960b5af27316d9383f51129e1029ddcb
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-26 09:04:32 -0300

    ex5 da semana 4

---
+++ Semana4/ex5/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        private int _idade;
+        public int Idade {
+            get { return _idade; }
+            set { 
+                if(value > 0) _idade = value;
+                else _idade = 0;
+            } 
+        }
+        public void DefinirIdade(int idade)
+        {
+            if(idade > 0) Idade = idade;
+        }
+    }
+}
---
+++ Semana4/ex5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Joao";
+            pessoa.Idade = -1;
+            Console.WriteLine(pessoa.Idade);
+            pessoa.DefinirIdade(10);
+            Console.WriteLine(pessoa.Idade);
+        }
+    }
+}
