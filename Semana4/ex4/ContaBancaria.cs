2638de13dafc56e59dc1ad4cbb5089c2ec6d211d
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-26 08:27:30 -0300

    ex4 da semana 4

---
+++ Semana4/ex4/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4
+{
+    internal class ContaBancaria
+    {
+        private double _saldo;
+
+        public ContaBancaria(double saldo)
+        {
+            if (saldo < 0) { saldo = 0; }
+            else Saldo = saldo;
+        }
+
+        public double Saldo { 
+            get => _saldo;
+            set {
+                if (_saldo < 0) { _saldo = 0; }
+                else _saldo = value; 
+            } 
+        }
+
+        public void Depositar(double valor)
+        {
+            if(valor >= 0)
+            {
+                Saldo += valor;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor >= 0 && valor <= Saldo)
+            {
+                Saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Semana4/ex4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria(-200.00);
+            Console.WriteLine(contaBancaria.Saldo.ToString("C"));
+            contaBancaria.Depositar(100.00);
+            Console.WriteLine(contaBancaria.Saldo.ToString("C"));
+            Console.WriteLine(contaBancaria.Sacar(100.00));
+            Console.WriteLine(contaBancaria.Saldo.ToString("C"));
+            
+        }
+    }
+}
