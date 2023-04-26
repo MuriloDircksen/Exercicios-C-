c0c1fc0f4464e0c84123c9edb28ced56a880ffec
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-26 09:19:12 -0300

    ex6 da semana 4

---
+++ Semana4/ex6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo1 = new Retangulo
+            {
+                Altura = -1,
+                Largura = 2,
+            };
+            Retangulo retangulo2 = new Retangulo { 
+                Altura= 5,
+                Largura = 2,
+            };
+
+            Console.WriteLine("Area: "+ retangulo1.CalculaArea());
+            Console.WriteLine("Area: " + retangulo2.CalculaArea());
+        }
+    }
+}
---
+++ Semana4/ex6/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex6
+{
+    internal class Retangulo
+    {
+        private double _altura;
+        public double Altura {
+            get { return _altura; } 
+            set {
+                if(value<0) _altura = 0;
+                else _altura = value;
+            } 
+        }
+        private double _largura;
+        public double Largura
+        {
+            get { return _largura; }
+            set
+            {
+                if (value < 0) _largura = 0;
+                else _largura = value;
+            }
+        }
+
+        public double CalculaArea()
+        {
+            return _largura*_altura;
+        }
+    }
+}
