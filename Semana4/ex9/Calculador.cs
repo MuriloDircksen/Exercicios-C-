ea82d4fc22f315876d94ec47ca205f71b12da094
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-27 10:05:17 -0300

    ex9 da semana 4

---
+++ Semana4/ex9/Calculador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex9
+{
+    static class Calculador
+    {
+        public const decimal Pi= 3.14159265359M;
+        
+        
+        public static Double Soma(double valor1, double valor2)
+        {
+            return valor1 + valor2;
+        }
+        public static Double Subtrai(double valor1, double valor2)
+        {
+            return valor1 - valor2;
+        }
+        public static Double Multiplica(double valor1, double valor2)
+        {
+            return valor1 * valor2;
+        }
+        public static Double Dividi(double valor1, double valor2)
+        {
+            if (valor2 == 0)
+            {
+                Console.WriteLine("Divisão por zero!");
+                return 0;
+            }
+            return valor1 / valor2;
+        }
+    }
+}
---
+++ Semana4/ex9/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soma 1 + 2: " + Calculador.Soma(1, 2));
+            Console.WriteLine("Subtrair 1 - 2: " + Calculador.Subtrai(1, 2));
+            Console.WriteLine("Multiplica 3 * 2: " + Calculador.Multiplica(3, 2));
+            Console.WriteLine("Dividi 2 / 2: " + Calculador.Dividi(2, 2));
+            Console.WriteLine("Dividi 2 / 0: " + Calculador.Dividi(2, 0));
+        }
+    }
+}
