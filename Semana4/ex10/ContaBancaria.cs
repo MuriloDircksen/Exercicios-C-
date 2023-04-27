4dd9968f19e32fc9cb88f479c72a4a3306ef25a6
Murilo Dircksen <murilo.dircksen@gmail.com>
2023-04-27 13:09:00 -0300

    ex10 da semana 4

---
+++ Semana4/ex10/ContaBancaria.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex10
+{
+    internal class ContaBancaria
+    {
+        private decimal _saldo;
+        public decimal Saldo { 
+            get { return _saldo; }
+            private set { _saldo = value; } 
+        }
+        private List<string> _registrarTransacao = new List<string>();
+
+        public void Depostio(decimal valor)
+        {
+            if(valor >= 0)
+            {
+                Saldo += valor;
+                _registrarTransacao.Add($"Depósito de: {valor}");
+            }
+            else { Console.WriteLine("Valor inválido");}
+        }
+        public bool Saque(decimal valor)
+        {
+
+            if(valor >= 0 && valor <= Saldo) { 
+                Saldo -= valor;
+                _registrarTransacao.Add($"Saque de: {valor}");
+                return true;
+            }
+            Console.WriteLine("Valor inválido"); 
+            return false;
+        }
+        public decimal ConsultaSaldo()
+        {
+            return Saldo;
+        }
+
+        public void HistoricoTransacoes()
+        {
+            foreach(string transacao in _registrarTransacao)
+            {
+                Console.WriteLine(transacao);
+            }
+           
+        }
+
+    }
+}
---
+++ Semana4/ex10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ContaBancaria contaBancaria = new ContaBancaria();
+
+            contaBancaria.Depostio(1000.00M);
+            Console.WriteLine("Saldo: " + contaBancaria.ConsultaSaldo());
+            contaBancaria.Saque(1200.00M);
+            Console.WriteLine("Saldo: " + contaBancaria.ConsultaSaldo());
+            contaBancaria.Saque(300.00M);
+            Console.WriteLine("Saldo: " + contaBancaria.ConsultaSaldo());
+            contaBancaria.Depostio(100.00M);
+            Console.WriteLine("Saldo: " + contaBancaria.ConsultaSaldo());
+            Console.WriteLine();
+            contaBancaria.HistoricoTransacoes();
+        }
+    }
+}
